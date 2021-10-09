9a1a439cb80c241c1e0c3333454433db3bf97569
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-10-09 14:46:21 -0500

    Check all and uncheck all feature implemented...

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -47,4 +47,19 @@ export class PizzaToppingsComponent implements OnInit {
       )
     ;
   }
+
+  checkAll = () => this.availablePizzaToppings = this.availablePizzaToppings.map(
+    x => ({
+      ...x
+      , checked: true
+    })
+  );
+
+  uncheckAll = () => this.availablePizzaToppings = this.availablePizzaToppings.map(
+    x => ({
+      ...x
+      , checked: false
+    })
+  );
+
 }
