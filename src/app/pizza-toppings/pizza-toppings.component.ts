9df782b26d076c7bca37032ab3ba9ce9b192542a
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-10-09 13:23:43 -0500

    Automatically calculating the total...

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -36,11 +36,11 @@ export class PizzaToppingsComponent implements OnInit {
     console.log(this.availablePizzaToppings);
   }
 
-  totalPrice = 0;
-
-  calculateTotal = () => {
-    this.totalPrice = this.availablePizzaToppings
-      .filter(x => x.checked)
+  get totalPrice() {
+    return this.availablePizzaToppings
+      .filter(
+        x => x.checked
+      )
       .reduce(
         (acc, x) => acc + x.price
         , 0
