6d3370112153c54741e902321c895178d9aba388
Gustave Gbossou <ame.gbossou87@gmail.com>
2022-02-26 16:18:57 -0600

    Boom!!! We're calling a DI service
    This is fun

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzaToppingsComponent implements OnInit {
 
-  constructor() { }
+  //Magic DI (Dependency injection)
+  constructor(private pizzaSvc:PizzaService
+    ) { }
 
   ngOnInit(): void {
+    const pt = this.pizzaSvc.getPizzaToppingFromTheCloud();
+    console.log(pt);
   }
 
 }
--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -4,6 +4,24 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PizzaService {
-
   constructor() { }
+
+  getPizzaToppingFromTheCloud(){
+    // We pretend me make webservice call here
+    const toppingFromWeb = [
+      {
+        name: 'Olives'
+        , price: 2.25
+      }
+      ,  {
+        name: 'Peperoni'
+        , price: 2.5
+      }
+      ,  {
+        name: 'Saucage'
+        , price: 1.25
+      }
+    ];
+    return toppingFromWeb;
+  }
 }
