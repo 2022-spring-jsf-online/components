37b7031267b1fa281324a59abd3f2c2bbdc45c41
Gustave Gbossou <ame.gbossou87@gmail.com>
2022-02-27 14:25:02 -0600

    Added pizzaToppingDisplay interface
    And transform data to the proper shape

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../pizza.service';
 
+// interface are named in Pascal case, will property are named in camel case
+interface PizzaToppingDisplay{ 
+  name:string;
+  price:number;
+  checked:boolean;
+}
+
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
@@ -10,11 +17,22 @@ export class PizzaToppingsComponent implements OnInit {
 
   //Magic DI (Dependency injection)
   constructor(private pizzaSvc:PizzaService
-    ) { }
+    ) { 
+
+    }
+    availablePizzaToppings: PizzaToppingDisplay[] = [];
 
   ngOnInit(): void {
     const pt = this.pizzaSvc.getPizzaToppingFromTheCloud();
     console.log(pt);
+    
+    // Transforming pt data to add checked property using map function
+    this.availablePizzaToppings = pt.map(x => ({
+      ...x
+      ,checked: false
+    })
+    );
+    console.log(this.availablePizzaToppings);
   }
 
 }
