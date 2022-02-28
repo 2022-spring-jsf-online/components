dfe018f0216e6938ff14823c8386adbd955fa098
rchua-matc <rchua@madisoncollege.edu>
2022-02-28 09:31:52 -0600

    Automatically calculating the total.
    and the results of npm install

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -36,15 +36,15 @@ export class PizzaToppingsComponent implements OnInit {
     console.log(this.availablePizzaToppings);
   }
 
-  totalPrice = 0;
-
-  calculateTotal = () => {
-    this.totalPrice = this.availablePizzaToppings
-      .filter(x => x.checked)
-      .reduce(
-        (acc, x) => acc + x.price
-        , 0
-      )
-    ;
-  }
+  get totalPrice() {
+      return this.availablePizzaToppings
+        .filter(
+          x => x.checked
+        )
+        .reduce(
+          (acc, x) => acc + x.price
+          , 0
+        )
+      ;
+  };
 }
