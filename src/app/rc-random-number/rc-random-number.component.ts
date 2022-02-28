96896291a55a9b2fad2c89cb2d08d3704efee37f
rchua-matc <rchua@madisoncollege.edu>
2022-02-28 11:11:55 -0600

    Implemented number generation logic
    Slack n' tell complete

--- src/app/rc-random-number/rc-random-number.component.ts
+++ src/app/rc-random-number/rc-random-number.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-rc-random-number',
-  templateUrl: './rc-random-number.component.html',
-  styleUrls: ['./rc-random-number.component.css']
+    selector: 'app-rc-random-number',
+    templateUrl: './rc-random-number.component.html',
+    styleUrls: ['./rc-random-number.component.css']
 })
 export class RcRandomNumberComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    min = 0;
+    max = 100;
+    randomNumber = 0;
+
+    ngOnInit(): void {
+    }
+
+    generate = () => this.randomNumber =
+            Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
 
 }
