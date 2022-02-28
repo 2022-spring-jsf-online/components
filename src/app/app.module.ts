4b10da672fe8fca4f232528faf979a6e253bbc93
rchua-matc <rchua@madisoncollege.edu>
2022-02-28 10:53:35 -0600

    Generated random number component
    ng generate component...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { RcRandomNumberComponent } from './rc-random-number/rc-random-number.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    RcRandomNumberComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/rc-random-number/rc-random-number.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rc-random-number',
+  templateUrl: './rc-random-number.component.html',
+  styleUrls: ['./rc-random-number.component.css']
+})
+export class RcRandomNumberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
