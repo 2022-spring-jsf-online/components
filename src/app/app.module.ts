7d152d783084bde99cd6cd3542bc9dadfbaf2b2a
ipoluboiarynov <77748323+ipoluboiarynov@users.noreply.github.com>
2022-03-02 17:54:18 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSlideToggle, MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { IpAdditionComponent } from './ip-addition/ip-addition.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    IpAdditionComponent
   ],
   imports: [
     BrowserModule
@@ -23,6 +26,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     , MatCardModule
     , MatButtonModule
     , MatCheckboxModule
+    , MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
