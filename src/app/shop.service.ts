dafc61cb1314f6aa16b446bbc9029c39f3372890
hkuhnau <95493237+hkuhnau@users.noreply.github.com>
2022-03-02 14:15:40 -0600

    data from service
    hard-coded the data in the service

--- src/app/shop.service.ts
+++ src/app/shop.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class ShopService {
 
   constructor() { }
+
+    getShoppingCartItems() {
+        const theCart = [
+            {
+                item: "book",
+                quantity: 1
+            },
+            {
+                item: "coffee mug",
+                quantity: 1
+            },
+            {
+                item: "pillow",
+                quantity: 1
+            }
+        ];
+        
+        return theCart;
+    }
 }
