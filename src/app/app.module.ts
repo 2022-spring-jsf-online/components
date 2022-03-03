d46a2319d863f4d8eee4123bb5e239de270bbf63
Gustave Gbossou <ame.gbossou87@gmail.com>
2022-03-03 12:28:34 -0600

    imported button, card, checkbox material design components
    Let see them in use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatCheckboxModule } from '@angular/material/checkbox';  
 
 @NgModule({
   declarations: [
@@ -12,7 +15,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule
-    ,FormsModule, BrowserAnimationsModule
+    ,FormsModule 
+    ,BrowserAnimationsModule
+    ,MatCardModule
+    ,MatButtonModule
+    ,MatCheckboxModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
