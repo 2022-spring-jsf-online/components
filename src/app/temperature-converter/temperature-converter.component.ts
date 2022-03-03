c7ded7b89b3f1d6d00381ebc90fc0b167c014a4e
Gustave Gbossou <ame.gbossou87@gmail.com>
2022-03-03 15:32:50 -0600

    Generated temperature-converter component
    This component will convert temperature in Celcius to Fahrenheit

---
+++ src/app/temperature-converter/temperature-converter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-temperature-converter',
+  templateUrl: './temperature-converter.component.html',
+  styleUrls: ['./temperature-converter.component.css']
+})
+export class TemperatureConverterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
