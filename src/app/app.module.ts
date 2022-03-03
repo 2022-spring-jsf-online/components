2d4b060bc048e89a3bdcb2f9574944b16c4ab22b
Gustave Gbossou <ame.gbossou87@gmail.com>
2022-03-03 15:32:50 -0600

    Generated temperature-converter component
    This component will convert temperature in Celcius to Fahrenheit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { TemperatureConverterComponent } from './temperature-converter/temperature-converter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    TemperatureConverterComponent
   ],
   imports: [
     BrowserModule
