6db91f1f1612f7a89e5bbe6e7d89399e3164d0e9
Alisa <amoua2@madisoncollege.edu>
2022-03-05 18:44:56 -0600

    Starting to build the my component
    imported the mat controls planned for usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { AmComponentComponent } from './am-component/am-component.component';
+import { MatChipModule } from '@angular/material/chips';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -25,6 +28,9 @@ import { AmComponentComponent } from './am-component/am-component.component';
     , MatCardModule
     , MatButtonModule
     , MatCheckboxModule
+    , MatChipModule
+    , MatFormFieldModule
+    , MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
