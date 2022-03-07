536096ffc3ad5a1ee7e708f282ed590c53bdd63e
Gustave Gbossou <ame.gbossou87@gmail.com>
2022-03-06 23:22:56 -0600

    Formula to convert temperature from Celsius to Fahrenheit is added
    The template is updated to display the
    Function to convert the temperature is added.
    Temperature after conversion

--- src/app/temperature-converter/temperature-converter.component.ts
+++ src/app/temperature-converter/temperature-converter.component.ts
@@ -1,3 +1,4 @@
+import { convertActionBinding } from '@angular/compiler/src/compiler_util/expression_converter';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,4 +13,15 @@ export class TemperatureConverterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  tempC = 0;
+  //Method for temperature conversion into Fahrenheit
+  convertTemp() {
+    return (this.tempC * 1.8) + 32;
+    //console.log((this.tempC * 1.8) + 32);
+  }
+  get tempF() {
+    return this.convertTemp();
+  } 
+  
+  
 }
