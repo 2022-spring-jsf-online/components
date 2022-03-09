ce499b88d5e42049760bc41b9e9eedca756546dc
eburzlaff <77722677+eburzlaff@users.noreply.github.com>
2022-03-09 03:04:34 -0600

    Finishing week 7 slack and tell

--- src/app/eb-addition/eb-addition.component.ts
+++ src/app/eb-addition/eb-addition.component.ts
@@ -12,4 +12,30 @@ export class EbAdditionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  numberOneAdd = 0;
+  numberOneSub = 0;
+  numberOneDiv = 0;
+  numberOneMult = 0;
+
+  numberTwoAdd = 0;
+  numberTwoSub = 0;
+  numberTwoDiv = 0;
+  numberTwoMult = 0;
+  
+  totalAdd = 0;
+  totalSub = 0;
+  totalDiv = 0;
+  totalMult = 0;
+
+  add = () => this.totalAdd = this.numberOneAdd + this.numberTwoAdd;
+
+  subtract = () => this.totalSub = this.numberOneSub - this.numberTwoSub;
+
+  divide = () => this.totalDiv = this.numberOneDiv / this.numberTwoDiv;
+
+  multiply = () => this.totalMult = this.numberOneMult * this.numberTwoMult;
+
+  
+
+
 }
