492d1346a0e35f52d8f336775885e9343ac2ec70
tekelsey012 <59896964+tekelsey012@users.noreply.github.com>
2022-03-11 16:42:34 -0600

    Returning to previous state
    Made error worse, going back to previous state.

--- src/app/tk-addition/tk-addition.component.ts
+++ src/app/tk-addition/tk-addition.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './tk-addition.component.html',
   styleUrls: ['./tk-addition.component.css']
 })
-export class TkAdditionComponent implements ngOnInit {
+export class TkAdditionComponent implements OnInit {
 
 export class AppComponent {
   toggle = true;
