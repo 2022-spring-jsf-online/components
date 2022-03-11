fe11748ebe5107759762be8e80fd468e8f690387
tekelsey012 <59896964+tekelsey012@users.noreply.github.com>
2022-03-11 16:12:38 -0600

    Added tk-addition component
    Added the needed component for Week 7

---
+++ src/app/tk-addition/tk-addition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tk-addition',
+  templateUrl: './tk-addition.component.html',
+  styleUrls: ['./tk-addition.component.css']
+})
+export class TkAdditionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
