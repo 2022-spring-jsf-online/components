f0fdc63bd225a4ba851241bf7ebe66087398fb0a
adiaz111 <adiaz8@madisoncollege.edu>
2022-03-12 13:42:19 -0600

    Starting to build the addition component...
    Remembered to import the mat controls I plan to use...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { AdAdditionComponent } from './ad-addition/ad-addition.component';
+import { MatInputModule } from '@angular/material/input';
+import { matFormFieldAnimations, MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -25,6 +27,8 @@ import { AdAdditionComponent } from './ad-addition/ad-addition.component';
     , MatCardModule
     , MatButtonModule
     , MatCheckboxModule
+    , MatInputModule
+    , MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
